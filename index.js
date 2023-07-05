3d5f387707384f22ec3adc6a06d6fe8b9a20ae6f
adamskid007 <83013775+adamskid007@users.noreply.github.com>
2023-07-05 14:35:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import http from "http"
 import express from "express"
 import translate from "translate-google"
+import cors from "cors"
 
 const PORT = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
 
+app.use(cors())
 app.use(express.static("public"))
 app.use(express.json())
 
